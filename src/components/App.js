1c5835676929e47d47956686e351f03ccc06afd0
ME <cheung.samuelyc@gmail.com>
2021-11-09 19:47:43 +0800

    Added intro

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/App.css';
+import React from 'react';
+import Header from './Header';
+import Intro from './Intro';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+    <div>
+        <h1>Hello<h1>
+        </h1>I'm Samuel.</h1>
+        <p> additional descriptive text</p>
+    </div>
+)
