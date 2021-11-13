0b0e9bc7b54a160735edba1c446059e35a9fe5be
ME <cheung.samuelyc@gmail.com>
2021-11-13 22:08:58 +0800

    added projects

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/ProjectBox.css';
 
-export default (props) => (
-    <div className='projectBox'>
+const ProjectBox = (props) => (
+    <div className='projectBox' onClick={() => { console.log('helo') }}>
         <h3> {props.name}</h3>
         <p>{props.desc}</p>
         <p>{props.repo}</p>
+    // add in external link for repo
     </div>
 )
+
+export default ProjectBox;
