e56433483edf0fb28ab152e4c25451ee2e4ae0c6
ME <cheung.samuelyc@gmail.com>
2021-11-19 03:13:06 +0800

    adjusted icon size

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,9 @@ export default () => (
     <div className='contact container'>
         <h1 className='contact-title'>Drop me a message below!</h1>
         <ContactForm />
-        <a href='https://www.linkedin.com/in/samuel-cheungyc/' ><i class="fab fa-linkedin fa-2x contact-icons" /></a>
-        <a href='https://github.com/salmonsashimi' ><i class="fab fa-github-square fa-2x contact-icons" /></a>
-        <a href='https://www.google.com' ><i class="fas fa-envelope-square fa-2x contact-icons" /></a>
+        <a href='https://www.linkedin.com/in/samuel-cheungyc/' ><i class="fab fa-linkedin fa-3x contact-icons" /></a>
+        <a href='https://github.com/salmonsashimi' ><i class="fab fa-github-square fa-3x contact-icons" /></a>
+        <a href='https://www.google.com' ><i class="fas fa-envelope-square fa-3x contact-icons" /></a>
     </div>
 )
 
