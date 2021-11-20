7714120fedfc631c1794f48838d75b0055da09c4
ME <cheung.samuelyc@gmail.com>
2021-11-20 19:47:23 +0800

    added date formatting

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import moment from 'moment';
 import SkillBox from './SkillBox';
 import '../styles/About.css'
 
+
 export default () => {
-    let months = 2000;
+    let today = moment();
+    let start = moment([2020, 10, 1]);
+    let difference = today.diff(start, 'months');
     return (
         <div className='about container'>
             <h1 className='about-title'>About Me</h1>
-            <p>I first stumbled onto programming when I decided to learn data analytics for self-development. Python was my language of choice then, and I went through an online course to learn it.</p>
-            <p>But we all know that Python is far more capable than just data analytics. I went from creating CLI functions and GUIs, to building live websites. The enjoyable process made me realise I want to pursue a career in this field.</p>
-
-            <p>From then on, I went through a variety of learning resources available online (thanks internet!), from platforms such as Udemy, Harvard/edX and Stack Overflow. I am a firm believer that anyone can learn something as long as they put their mind to it. </p>
-            <p>As of today, I have been self-learning to code for <span className='months'>{months}</span> months. Currently, I am working on a online ecommerce application to hone my skills in React.</p>
+            <p>I first stumbled onto programming when I decided to pick up data analytics for self-development. Python was my language of choice then, and I purchased an online course to learn it.</p>
+            <p>But we all know that Python is far more capable than just data analytics. I went from creating CLI functions and GUIs, to building live websites. The enjoyable process made me realise I want to pursue a career in software development.</p>
+            <p>I am a firm believer that nothing is impossible as long as you put their mind to it. I was able to upgrade myself through the wide variety of learning resources available online, with many thanks to platforms such as Udemy, Harvard/edX and Stack Overflow. </p>
+            <p>As of today, I have been programming for <span className='months'>{difference}</span> months. Currently, I am working on a online ecommerce application to hone my skills in React.</p>
             <h1>These are the technologies I frequent:</h1>
             <SkillBox />
-            <h1>Lengends:</h1>
-
         </div>
     )
 }
