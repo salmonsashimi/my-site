5a023f824517805b2acbe5c0df1c4efbf14fde21
ME <cheung.samuelyc@gmail.com>
2021-11-24 11:20:09 +0800

    remove filler text

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -6,7 +6,6 @@ const ProjectBox = (props) => (
         <div className='projectBox-text'>
             <h3> {props.name}</h3>
             <p>{props.desc}</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus est pellentesque elit ullamcorper dignissim cras. Consectetur a erat nam at.</p>
         </div>
         <div className='projectBox-icons'>
             {props.link && <a href={props.link} ><i class="fas fa-external-link-square-alt fa-2x projectBox-icon" /></a>}
