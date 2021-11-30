fdd788dfd428f77a4827f4cbbf734bfbf658cdcf
ME <cheung.samuelyc@gmail.com>
2021-11-30 16:00:04 +0800

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ export default () => {
     return (
         <div className='about container'>
             <h1 className='about-title'>About Me</h1>
-            <p>I first stumbled onto programming through an online Python course. My primary intention was to equip myself with data analytics for my personal development.</p>
+            <p>I first stumbled onto programming through an online Python course, with the intention of equiping myself with data analysis skills.</p>
             <p>But we all know that Python is far more capable than just data analytics. I went from creating CLI functions and GUIs, to building live websites. The enjoyable process made me realise I want to pursue a career in software development.</p>
             <p>I am a firm believer that nothing is impossible as long as you put their mind to it. I was able to upgrade myself through the wide variety of learning resources available online, with many thanks to platforms such as Udemy, Harvard/edX and Stack Overflow. </p>
             <p>As of today, I have been programming for <span className='months'>{difference}</span> months. I am currently working on a online ecommerce application to hone my skills in React.</p>
