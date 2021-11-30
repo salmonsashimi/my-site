fa3456885a607951e3996e674378c9171fc1349a
ME <cheung.samuelyc@gmail.com>
2021-11-30 15:45:35 +0800

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ export default () => {
             <p>But we all know that Python is far more capable than just data analytics. I went from creating CLI functions and GUIs, to building live websites. The enjoyable process made me realise I want to pursue a career in software development.</p>
             <p>I am a firm believer that nothing is impossible as long as you put their mind to it. I was able to upgrade myself through the wide variety of learning resources available online, with many thanks to platforms such as Udemy, Harvard/edX and Stack Overflow. </p>
             <p>As of today, I have been programming for <span className='months'>{difference}</span> months. I am currently working on a online ecommerce application to hone my skills in React.</p>
-            <h1>These are the technologies I frequent:</h1>
+            <h1>Technologies I frequent:</h1>
             <SkillBox />
         </div>
     )
