44c13c42df21144439a28547ebf4a59ef2e0fb07
ME <cheung.samuelyc@gmail.com>
2021-12-02 17:43:41 +0800

    added intro responsiveness

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,8 @@ export default () => (
         <div className='css-typing'>
             <h1>Hello!</h1>
             <h1>I'm Samuel Cheung,</h1>
-            <h1>an aspiring software developer.</h1>
+            <h1>an aspiring software <span className='web'>developer.</span></h1>
+            <h1 className='mobile'>developer.</h1>
             <a href='https://www.linkedin.com/in/samuel-cheungyc/' ><i class="fab fa-linkedin fa-2x intro-icons" /></a>
             <a href='https://github.com/salmonsashimi' ><i class="fab fa-github-square fa-2x intro-icons" /></a>
             <a href='https://www.google.com' ><i class="fas fa-envelope-square fa-2x intro-icons" /></a>
