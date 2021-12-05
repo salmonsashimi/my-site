82c14fc0f1114ee07e1a2bd465a55e7c5fff27bc
ME <cheung.samuelyc@gmail.com>
2021-12-05 14:45:52 +0800

    editted about

--- src/components/About.js
+++ src/components/About.js
@@ -13,8 +13,8 @@ export default () => {
             <h1 className='about-title'>About Me</h1>
             <p>I first stumbled onto programming through an online Python course, with the intention of equiping myself with data analysis skills.</p>
             <p>But we all know that Python is far more capable than just data analytics. I went from creating CLI functions and GUIs, to building live websites. The enjoyable process made me realise I want to pursue a career in software development.</p>
-            <p>I am a firm believer that nothing is impossible as long as you put their mind to it. I was able to upgrade myself through the wide variety of learning resources available online, with many thanks to platforms such as Udemy, Harvard/edX and Stack Overflow. </p>
-            <p>As of today, I have been programming for <span className='months'>{difference}</span> months. I am currently working on a online ecommerce application to hone my skills in React.</p>
+            <p>I am a firm believer that nothing is impossible as long as you put your mind to it. I was able to upgrade myself through the wide variety of learning resources available online, with many thanks to platforms such as Udemy, Harvard/edX and Stack Overflow. </p>
+            <p>As of today, I have been programming for <span className='months'>{difference}</span> months. Currently, I am working on a online ecommerce application to hone my skills in React.</p>
             <h1>Technologies I frequent:</h1>
             <SkillBox />
         </div>
